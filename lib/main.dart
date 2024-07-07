310de4a2ff257e784c98ef194006be074ef8d10d
Jigya <jigyamunjal@gmail.com>
2024-07-08 05:25:01 +0530

    Edit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 136, 99, 201),
         body: Center(
-          child: Text('Hello'),
+          child: Text('Hello World!'),
         ),
       ),
     ),
