554574054e76cb030149d19b43f5b92f92746aac
Jigya <jigyamunjal@gmail.com>
2024-07-08 05:19:01 +0530

    Dice App First Edit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 136, 99, 201),
+        body: Center(
+          child: Text('Hello'),
+        ),
+      ),
+    ),
+  );
+}
+
+// void runApp(){
+
+// }
